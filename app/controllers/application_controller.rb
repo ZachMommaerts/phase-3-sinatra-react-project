9401033c3a12d005de779d59456ec829f0aea2d0
Zach <zmommaerts@gmail.com>
2022-01-06 17:06:14 -0600

    added leaderboard route for backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,7 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get '/leaderboards' do
+    Score.all.sort{ |a,b| b.score <=> a.score}.to_json(includes: :username)
+  end
 end
