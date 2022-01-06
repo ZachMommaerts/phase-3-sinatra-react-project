9e6f6d711c5b9ae433b4112513da2ffd00712fed
Zach <zmommaerts@gmail.com>
2022-01-06 16:55:12 -0600

    added seed data and model/model dependencies

---
+++ app/models/username.rb
@@ -0,0 +1,3 @@
+class Username < ActiveRecord::Base
+    has_many :scores
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+Username.create(name: '7331_GAMER')
+Username.create(name: 'cutey-patootie')
+Username.create(name: 'xXSnIpEr-GoDXx')
+Username.create(name: 'SSJ-SuperKid')
+
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
+Score.create(score: rand(0..10), username_id: rand(1..4))
 
 puts "✅ Done seeding!"
