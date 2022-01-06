a4f08323a57ee9d2dda034bb1f95ba2c8f3514ad
Akeem <AkeemS11788@gmail.com>
2022-01-06 11:12:09 -0500

    first backend commit

---
+++ lib/billboard_top_100.rb
@@ -0,0 +1,17 @@
+class BillboardTop100
+    attr_accessor :songname, :artist
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+        @@all.clear
+    end
+    
+end
---
+++ lib/scraper.rb
@@ -0,0 +1,39 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+require_relative './billboard_top_100.rb'
+
+class Scraper
+
+    def get_webpage
+        doc = Nokogiri::HTML(open("https://www.billboard.com/charts/hot-100/"))
+        
+    end
+
+    def top100_tracks
+        # self.get_webpage.css(".o-chart-results-list-row-container")
+        self.get_webpage.css(".o-chart-results-list__item")
+    end
+
+    def create_songs
+        self.top100_tracks.each do |music|
+            song = BillboardTop100.new
+            song.songname = music.css("h3").text
+            song.artist = music.css(".c-label").text
+        end
+    end
+
+    def print_songs
+        self.create_songs
+        BillboardTop100.all.each do |song|
+            if song.songname && song.songname != ""
+                puts "Song: #{song.songname}"
+                puts "Artist: #{song.artist}"
+            end
+        end
+    end
+    binding.pry
+end
+
+Scraper.new.print_songs
