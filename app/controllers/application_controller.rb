a3b77ce9806b5cbdd90c2a8242c1c1a38a024b18
Zach <zmommaerts@gmail.com>
2022-01-06 22:32:14 -0600

    rearranged routing, scraper is going off multiple times - needs to be looked into

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,23 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    Song.destroy_all
+
+    scraper = Scraper.new
+    songs = scraper.print_songs
+    Song.create_from_collection(songs)
   end
 
   get '/leaderboards' do
     Score.all.sort{ |a,b| b.score <=> a.score}.to_json(include: :username)
   end
 
-  get '/songs/:id' do
-    Song.destroy_all
-
-    scraper = Scraper.new
-    songs = scraper.print_songs
-    Song.create_from_collection(songs)
-
-    Song.find(params[:id]).to_json
+  get '/random_song' do
+    Song.all.rawQuery("SELECT * FROM songs ORDER BY RANDOM() LIMIT 1", null);
   end
 
   post '/players' do
-    player = Username.create(name: params[:username])
+    player = Username.where(name: params[:username]).first_or_create
     Score.create(score: 0, username_id: player.id)
     player.to_json
   end
